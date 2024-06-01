52613a3263d9235518958dc2519afbc560c63493
AhmettCelik <140803207+AhmettCelik@users.noreply.github.com>
2024-06-02 00:37:17 +0300

    add reverseInput function

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ const checkButton = document.getElementById("check-btn");
 const palindromeInput = document.getElementById("text-input");
 const palindromeResultParagraph = document.getElementById("palindrome-result");
 
+let finalInput;
+
 const removeNonAlphabeticChars = str => str.replace(/[^a-zA-Z]/g, '');
+const reverseInput = (input) => removeNonAlphabeticChars(input).split("").reverse().join("");
