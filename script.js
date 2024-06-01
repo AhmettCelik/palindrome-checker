bc8e514d45e3f8403aed8de05d57f2ebd35466ae
AhmettCelik <140803207+AhmettCelik@users.noreply.github.com>
2024-06-02 00:56:52 +0300

    check and return result

--- script.js
+++ script.js
@@ -3,13 +3,27 @@ const palindromeInput = document.getElementById("text-input");
 const palindromeResultParagraph = document.getElementById("palindrome-result");
 const validationParagraph = document.getElementById("validation-paragraph");
 
+let firstValue;
+let secondValue;
+
 const removeNonAlphabeticChars = str => str.replace(/[^a-zA-Z]/g, '');
 const reverseInput = (input) => removeNonAlphabeticChars(input).split("").reverse().join("");
+
+const checkPalindrome = (firstValue, secondValue) => firstValue === secondValue ? true : false;
 
 const handleButtonClick = () => {
     if(palindromeInput.value === "") {
         validationParagraph.style.display = "block";
-    }else validationParagraph.style.display = "none";
+        palindromeResultParagraph.innerText = ""
+    }else {
+        validationParagraph.style.display = "none";
+        firstValue = reverseInput(palindromeInput.value);
+        secondValue = removeNonAlphabeticChars(palindromeInput.value);
+        if(checkPalindrome(firstValue, secondValue)) {
+            palindromeResultParagraph.innerText = "Yes! It's a palindrome!"
+        }else palindromeResultParagraph.innerText = "No! It's not a palindrome"
+    }
+        
 }
 
 checkButton.addEventListener("click", handleButtonClick)
