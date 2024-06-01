72176123427979b8df408c287f7fbe03eb6864ce
AhmettCelik <140803207+AhmettCelik@users.noreply.github.com>
2024-06-02 00:47:26 +0300

    add validation for empty input

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const checkButton = document.getElementById("check-btn");
 const palindromeInput = document.getElementById("text-input");
 const palindromeResultParagraph = document.getElementById("palindrome-result");
-
-let finalInput;
+const validationParagraph = document.getElementById("validation-paragraph");
 
 const removeNonAlphabeticChars = str => str.replace(/[^a-zA-Z]/g, '');
 const reverseInput = (input) => removeNonAlphabeticChars(input).split("").reverse().join("");
+
+const handleButtonClick = () => {
+    if(palindromeInput.value === "") {
+        validationParagraph.style.display = "block";
+    }else validationParagraph.style.display = "none";
+}
+
+checkButton.addEventListener("click", handleButtonClick)
