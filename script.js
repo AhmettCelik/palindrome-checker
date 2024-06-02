2da827bf6ec2780e7abf7e2d8edd6a9893f91894
AhmettCelik <140803207+AhmettCelik@users.noreply.github.com>
2024-06-02 13:11:48 +0300

    final adjustments

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const checkButton = document.getElementById("check-btn");
 const palindromeInput = document.getElementById("text-input");
-const palindromeResultParagraph = document.getElementById("palindrome-result");
+const palindromeResultParagraph = document.getElementById("result");
 const validationParagraph = document.getElementById("validation-paragraph");
 
 let firstValue;
 let secondValue;
 
-const removeNonAlphabeticChars = str => str.replace(/[^a-zA-Z]/g, '');
+const removeNonAlphabeticChars = str => str.replace(/[^a-zA-Z0-9]/g, '');
 const reverseInput = (input) => removeNonAlphabeticChars(input).split("").reverse().join("");
 
-const checkPalindrome = (firstValue, secondValue) => firstValue === secondValue ? true : false;
+const checkPalindrome = (firstValue, secondValue) => firstValue.toLowerCase() === secondValue.toLowerCase() ? true : false;
 
 const handleButtonClick = () => {
     if(palindromeInput.value === "") {
+        alert("Please input a value")
         validationParagraph.style.display = "block";
         palindromeResultParagraph.innerText = ""
     }else {
@@ -20,8 +21,8 @@ const handleButtonClick = () => {
         firstValue = reverseInput(palindromeInput.value);
         secondValue = removeNonAlphabeticChars(palindromeInput.value);
         if(checkPalindrome(firstValue, secondValue)) {
-            palindromeResultParagraph.innerText = "Yes! It's a palindrome!"
-        }else palindromeResultParagraph.innerText = "No! It's not a palindrome"
+            palindromeResultParagraph.innerText = `${palindromeInput.value} is a palindrome`
+        }else palindromeResultParagraph.innerText = `${palindromeInput.value} is not a palindrome`
     }
         
 }
